018b2edd5b57700dc4382498abdeecf33650c1c7
s1260237 <s1260237@ilab2dc10.u-aizu.ac.jp>
2019-07-11 16:12:18 +0900

    initial revision

---
+++ dis.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class dis{
+    public static void main(String[] args){
+	int randomValue1=0;
+	int randomValue2=0;
+
+	Random r1 = new Random();
+	Random r2 = new Random();
+
+	while(randomValue1 == 0 || randomValue2 == 0){
+	    randomValue1 = r1.nextInt(6);
+	    randomValue2 = r2.nextInt(6);
+	}
+	int total = randomValue1 + randomValue2;
+	System.out.println("Rolling the dice...");
+	System.out.println("Die 1:"+randomValue1);
+	System.out.println("Die 2:"+randomValue2);
+	System.out.println("Total value:"+total);
+
+    
+    }
+}
